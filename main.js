4b93ac61c05921a76b04cc9f801bc3e51f25d25f
David Folk <davidwfolk@gmail.com>
2020-06-15 11:12:00 -0700

    frontend v2 Done

---
+++ main.js
@@ -0,0 +1,34 @@
+let food = 0
+let autoFood = 0
+let eating = {
+  "eat": 1
+}
+
+let clickUpGrades = {
+  dots: {
+    name: "dots",
+    price: 50,
+    quantity: 0,
+    multipler: 1
+  },
+  skittles: {
+    name: "skittles",
+    price: 100,
+    quantity: 0,
+    multiplier: 2,
+  }
+}
+  let autoUpgrades = {
+    iceCream: {
+      name: "iceCream",
+      price: 250,
+      quantity: 0,
+      multiplier: 20
+    },
+    cottonCandy
+  }
+
+
+
+
+}
